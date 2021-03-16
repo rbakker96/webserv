b7d0ba541dcd609efd557f201f782a6a952495a7
qingqingqingli <kadlqbetty@gmail.com>
2021-03-16 18:04:53 +0100

    qli - update practice socket programming

---
+++ cmake-build-debug/CMakeFiles/webserv.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/webserv.dir/practice_socket_programming/client.c.o"
+  "CMakeFiles/webserv.dir/practice_socket_programming/server.c.o"
+  "webserv"
+  "webserv.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/webserv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
