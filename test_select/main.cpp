5d17b7c716205cfc444689508e790ba2c3be3fea
gbouwen <gijsbouwen@gmail.com>
2021-04-06 13:44:49 +0200

    rename variable save_fd buffer_fd

--- test_select/main.cpp
+++ test_select/main.cpp
@@ -15,7 +15,7 @@ int	accept_request(tcp_connection *connection)
 {
 	int	request_fd;
 
-	std::cout << "activity on port 8080" << std::endl;
+	std::cout << "activity on port 8080..." << std::endl;
 	request_fd = accept(connection->tcp_socket, (struct sockaddr *)&connection->addr, (socklen_t *)&connection->addr_len);
 	fcntl(request_fd, F_SETFL, O_NONBLOCK);
 	return (request_fd);
@@ -63,23 +63,23 @@ void	write_response(int request_fd, char *buffer)
 int	main(void)
 {
 	fd_set			read_fds, write_fds;
-	fd_set			save_read_fds, save_write_fds;
+	fd_set			buffer_read_fds, buffer_write_fds;
 	tcp_connection	connection_one;
 	int				highest_fd = -1, request_fd = -1, file_fd = -1;
 	char			requested_file_buffer[30000] = {0};
 
 	FD_ZERO(&read_fds);
 	FD_ZERO(&write_fds);
-	FD_ZERO(&save_read_fds);
-	FD_ZERO(&save_write_fds);
+	FD_ZERO(&buffer_read_fds);
+	FD_ZERO(&buffer_write_fds);
 	connection_one.create_socket();
 	connection_one.bind_socket_address(8080);
 	connection_one.create_connection(100);
 	highest_fd = calc_highest_fd(highest_fd, connection_one.tcp_socket);
 	while (1)
 	{
-		read_fds = save_read_fds;
-		write_fds = save_write_fds;
+		read_fds = buffer_read_fds;
+		write_fds = buffer_write_fds;
 		FD_SET(connection_one.tcp_socket, &read_fds);
 		std::cout << "before select" << std::endl;
 		int	select_value = select(highest_fd + 1, &read_fds, &write_fds, NULL, NULL);
@@ -92,27 +92,28 @@ int	main(void)
 			if (FD_ISSET(connection_one.tcp_socket, &read_fds))
 			{
 				request_fd = accept_request(&connection_one);
-				FD_SET(request_fd, &save_read_fds);
+				FD_SET(request_fd, &buffer_read_fds);
 				highest_fd = calc_highest_fd(highest_fd, request_fd);
 			}
 			if (FD_ISSET(request_fd, &read_fds))
 			{
 				read_request(request_fd);
-				FD_CLR(request_fd, &save_read_fds);
-				FD_SET(request_fd, &save_write_fds);
+				FD_CLR(request_fd, &buffer_read_fds);
 				file_fd = open_requested_file();
 				highest_fd = calc_highest_fd(highest_fd, file_fd);
-				FD_SET(file_fd, &save_read_fds);
+				FD_SET(file_fd, &buffer_read_fds);
 			}
 			if (FD_ISSET(file_fd, &read_fds))
 			{
+				std::cout << "check if requested file is ready to read..." << std::endl;
 				read(file_fd, requested_file_buffer, 30000);
-				FD_CLR(file_fd, &save_read_fds);
+				FD_CLR(file_fd, &buffer_read_fds);
+				FD_SET(request_fd, &buffer_write_fds);
 			}
 			if (FD_ISSET(request_fd, &write_fds))
 			{
 				write_response(request_fd, requested_file_buffer);
-				FD_CLR(request_fd, &save_write_fds);
+				FD_CLR(request_fd, &buffer_write_fds);
 				close(request_fd);
 				request_fd = -1;
 			}
