5bcd161a89b4a3ec773c313b5f5e57f2a57da7a5
Gijs Bouwen <gijsbouwen@gmail.com>
2021-05-18 15:00:22 +0200

    added verify_file_location_functions

--- webserver/helper/helper.hpp
+++ webserver/helper/helper.hpp
@@ -6,7 +6,7 @@
 /*   By: roybakker <roybakker@student.codam.nl>       +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/03/30 16:30:47 by roybakker     #+#    #+#                 */
-/*   Updated: 2021/05/18 12:37:08 by gbouwen       ########   odam.nl         */
+/*   Updated: 2021/05/18 14:56:20 by gbouwen       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,9 @@
 #include <cstdlib> // for malloc && free && exit
 #include <unistd.h>
 #include <iostream>
+#include <sys/stat.h>
+
+#include "../server/location_context.hpp"
 
 # define RESET			"\033[0m"
 # define CYAN			"\033[36m"
@@ -42,13 +45,19 @@ char						*ft_strjoin(char const *s1, char const *s2);
 
 //parse functions
 std::vector<std::string>	parse_vector(const std::string& str);
-std::string					parse_string (const std::string& str);
-int 						parse_number (const std::string& str);
-void 						parse_invalid (const std::string& str);
+std::string					parse_string(const std::string& str);
+int 						parse_number(const std::string& str);
+void 						parse_invalid(const std::string& str);
 vector                      str_to_vector(std::string request);
 std::string                 read_browser_request(int fd);
 
 //signal handler function
 void						signal_handler(int signal);
+
+//verify file location functions
+std::string					remove_duplicate_forward_slashes(std::string location);
+std::string					get_file(location_context location_block, std::string location);
+std::string					skip_first_directory(std::string uri_location);
+std::string					get_first_directory(std::string uri_location);
 
 #endif
